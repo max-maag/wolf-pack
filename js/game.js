f88777329db2ab10ab4b51454331b478f326a51f
keinBaum <jasoco23>
2014-12-08 19:49:49 +0100

    Fixed game not starting

--- js/game.js
+++ js/game.js
@@ -83,9 +83,7 @@
       document.addEventListener("keyup", this.playerController.handleInput);
       this.animals = [this.playerController];
       this.loader.onComplete = function() {
-        return requestAnimFrame(function() {
-          return this.step;
-        });
+        return requestAnimFrame(this.step);
       };
       return this.loader.load();
     };
