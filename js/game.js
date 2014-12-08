e168fb73d78266fb64b84932ff80e67bc132c509
keinBaum <jasoco23>
2014-12-08 19:39:58 +0100

    Fixed missing ->

--- js/game.js
+++ js/game.js
@@ -75,7 +75,9 @@
       document.addEventListener("keyup", this.playerController.handleInput);
       this.animals = [this.playerController];
       this.loader.onComplete = function() {
-        return requestAnimFrame(this.step);
+        return requestAnimFrame(function() {
+          return this.step;
+        });
       };
       this.loader.load();
     }
